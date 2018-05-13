73594edb86874038100635f0d8a84c0ebf3ff841
T.J. Hooker <tyrelhooker@gmail.com>
2018-05-13 19:17:10 -0400

    added table-cli npm and working display products and initial prompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+var Table = require("cli-table");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -9,27 +10,42 @@ var connection = mysql.createConnection({
   database: "bamazon_db"
 });
 
+var table = new Table({
+  head: ["Item Id", "Product Name", "Price"]
+});
+
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  start();
+  displayProducts();
 });
+
+function displayProducts() {
+  connection.query("SELECT * FROM products", function(err, res, fields) {
+    if (err) throw err;
+    res.forEach(function(res) {
+      table.push(
+        [res.item_id, res.product_name, res.price]
+      );
+    });
+    console.log(table.toString());
+    start();
+  });
+};
 
 function start() {
   inquirer
     .prompt({
       name: "ask",
-      type: "rawlist",
+      type: "list",
       message: "Would you like to purchase an item?",
       choices: ["YES", "NO"]
     })
     .then(function(answer) {
       if (answer.ask.toUpperCase() === "YES") {
-        purchaseProduct();
       }
       else {
         console.log("You are missing out on some great products!");
-
       }
     })
 }
@@ -42,9 +58,3 @@ function purchaseProduct() {
   });
 }
 
-function readProducts() {
-  connection.query("SELECT * FROM products", function(err, res) {
-    if (err) throw err;
-    console.log(res);
-  });
-}
