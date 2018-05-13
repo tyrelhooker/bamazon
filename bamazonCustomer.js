046b657ab871c47c78bdaf82500faf6aa54d3327
T.J. Hooker <tyrelhooker@gmail.com>
2018-05-12 22:45:18 -0400

    add npm packets to js file. Add preliminary inquirer prompts and functions.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,4 +12,39 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
+  start();
 });
+
+function start() {
+  inquirer
+    .prompt({
+      name: "ask",
+      type: "rawlist",
+      message: "Would you like to purchase an item?",
+      choices: ["YES", "NO"]
+    })
+    .then(function(answer) {
+      if (answer.ask.toUpperCase() === "YES") {
+        purchaseProduct();
+      }
+      else {
+        console.log("You are missing out on some great products!");
+
+      }
+    })
+}
+
+function purchaseProduct() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    console.log(res);
+    connection.end();
+  });
+}
+
+function readProducts() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    console.log(res);
+  });
+}
