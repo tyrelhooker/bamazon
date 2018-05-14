c33b9aeb4a7ffa30c4d3e7bab41f365b43cb5374
T.J. Hooker <tyrelhooker@gmail.com>
2018-05-14 02:33:31 -0400

    added comments and misc. edits

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,15 +10,13 @@ var connection = mysql.createConnection({
   database: "bamazon_db"
 });
 
-var table = new Table({
-  head: ["Item Id", "Product Name", "Price"]
-});
-
+// Starts connection to DB
 connection.connect(function(err) {
   if (err) throw err;
   start();
 });
 
+// Prompts user to purchase products
 function start() {
   inquirer
     .prompt({
@@ -37,7 +35,11 @@ function start() {
     });
 }
 
+// Creates CLI Table with DB values
 function displayProducts() {
+  var table = new Table({
+    head: ["Item Id", "Product Name", "Price"]
+  });
   var query = connection.query("SELECT * FROM products", function(err, res){
     if (err) throw err;
     res.forEach(function(res) {
@@ -48,9 +50,9 @@ function displayProducts() {
     console.log(table.toString());
     purchaseProduct();
   });
-  console.log(query.sql);
 };
 
+// Takes two user purchase inputs and compares the input to the DB stock.
 function purchaseProduct() {
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
@@ -68,9 +70,7 @@ function purchaseProduct() {
         }
       ])
       .then(function(answer){
-        // console.log(answer.productChoice);
-        // console.log(answer.productAmt);
-        
+        // Creates variables with the answers from user input
         var chosenItem;
         var custProd = parseInt(answer.productChoice);
         var custProdAmt = parseInt(answer.productAmt);
@@ -81,28 +81,26 @@ function purchaseProduct() {
             chosenItem = res;
           }
         });
-        // console.log(chosenItem.stock_quantity);
-
+        // Compares chosen product amt with DB stock values
         if (chosenItem.stock_quantity < custProdAmt) {
           console.log("We are currently sold out of this item. We currently only have " + chosenItem.stock_quantity + " in stock. Please select another item");
           purchaseProduct();
         }
         else {
+          // Variable holds the total amount of user's purchase
           var total = chosenItem.price * custProdAmt;
-
           console.log("\nThank you for your purchase. We are shipping " + custProdAmt + " " + chosenItem.product_name + " to you!\n");
           console.log("\nYour total cost is: " + total + "\n");
-
+          // Creates variable to pass into updateStock()
           var updateStock = parseInt(chosenItem.stock_quantity) - custProdAmt;
-          // console.log(updateStock);
-          updateProductQuantity(updateStock, custProd); 
+          updateStock(updateStock, custProd); 
         }
       }
     );
   });
 }
-
-function updateProductQuantity(updateStock, custProd) {
+// Updates the stock in DB after user purchase
+function updateStock(updateStock, custProd) {
   connection.query(
     "UPDATE products SET ? WHERE ?",
     [
@@ -120,7 +118,7 @@ function updateProductQuantity(updateStock, custProd) {
     }
   );
 }
-
+// Prompts user for another purchase.
 function anotherPurchase() {
   inquirer
     .prompt({
